b5bcf0d93047b5c312acdc01d4f9f10d9e22f790
Alesia <dogar_alesia@yahoo.com>
2026-01-14 08:59:24 +0200

    commit task 3 si 4 pt ca le am fct de o data

--- src/main/java/org/example/Service/VehicleService.java
+++ src/main/java/org/example/Service/VehicleService.java
@@ -3,6 +3,9 @@ package org.example.Service;
 import org.example.Model.Vehicle;
 import org.example.Repository.IRepository;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class VehicleService {
@@ -20,7 +23,42 @@ public class VehicleService {
                 .forEach(System.out::println);
     }
 
+    //. (0.5 Punkte) Sortierung der Fahrzeuge
+    //Sortieren Sie die Liste aller Fahrzeuge wie folgt:
+    //● aufsteigend nach ownerCity
+    //● bei gleichem ownerCity absteigend nach id
+    //Geben Sie die sortierte Liste anschließend auf der
+    //Konsole aus. Die Ausgabe erfolgt im gleichen Format wie
+    //in Aufgabe 1.
+    //. (1 Punkt)  Schreiben in eine Datei
+    //Schreiben Sie die in Aufgabe 3 sortierte Liste der
+    //Fahrzeuge in die Datei vehicles_sorted.txt. Jeder
+    //Fahrzeuge soll in einer eigenen Zeile gespeichert werden,
+    //im selben Format wie bei der Konsolenausgabe.
 
+    // public void task3And4SortAndSave() {
+    ////        List<Tribut> sorted = repo.findAll().stream()
+    ////                .sorted(Comparator.comparing(Tribut::getSkillLevel).reversed()
+    ////                        .thenComparing(Tribut::getName))
+    ////                .toList();
+    ////
+    ////        sorted.forEach(System.out::println);
+    ////
+    ////        try (PrintWriter pw = new PrintWriter(new FileWriter("tributes_sorted.txt"))) {
+    ////            for (Tribut t : sorted) pw.println(t);
+    ////        } catch (IOException e) { e.printStackTrace(); }
+    ////    }
+    //
 
+    public void task3And4SortAndSave(){
+        List<Vehicle> sorted = repo.findAll().stream()
+                .sorted(Comparator.comparing(Vehicle::getOwnerCity)
+                        .thenComparing(Vehicle::getId).reversed())
+                .toList();
+        sorted.forEach(System.out::println);
 
+        try (PrintWriter pw = new PrintWriter(new FileWriter("tributes_sorted.txt"))) {
+            for (Vehicle v : sorted) pw.println(v);
+                 } catch (IOException e) { e.printStackTrace(); }
+}
 }
